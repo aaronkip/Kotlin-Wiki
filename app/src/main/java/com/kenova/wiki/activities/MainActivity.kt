eb561e01e4224f1a1b9edc5e63009438427640b5
Akron254 <aaronrono42@gmail.com>
2020-04-19 18:08:48 +0300

    Clearing up fragments setup

--- app/src/main/java/com/kenova/wiki/activities/MainActivity.kt
+++ app/src/main/java/com/kenova/wiki/activities/MainActivity.kt
@@ -7,23 +7,46 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.kenova.wiki.R
+import com.kenova.wiki.fragments.ExploreFragment
+import com.kenova.wiki.fragments.FavoritesFragment
+import com.kenova.wiki.fragments.HistoryFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val exploreFragment : ExploreFragment
+    private val historyFragment : HistoryFragment
+    private val favoritesFragment : FavoritesFragment
+
+    init {
+        exploreFragment = ExploreFragment()
+        historyFragment = HistoryFragment()
+        favoritesFragment = FavoritesFragment()
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {item ->
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+
+        when(item.itemId)
+        {
+            R.id.navigation_explore -> transaction.replace(R.id.fragment_container, exploreFragment)
+            R.id.navigation_history -> transaction.replace(R.id.fragment_container, historyFragment)
+            R.id.navigation_favorites -> transaction.replace(R.id.fragment_container, favoritesFragment)
+        }
+
+        transaction.commit()
+        true
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, exploreFragment)
     }
 }
