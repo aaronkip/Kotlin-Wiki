b7230b7cdaaa7467d86383e26594c9c459156fde
Akron254 <aaronrono42@gmail.com>
2020-04-20 07:50:02 +0300

    Added page & query data models

---
+++ app/src/main/java/com/kenova/wiki/models/WikiPage.kt
@@ -0,0 +1,12 @@
+package com.kenova.wiki.models
+
+/**
+Created by Aaron at 4/20/2020
+Copyright (c) 2020 Kenova
+ **/
+class WikiPage {
+    var pageid: Int? = null
+    var title: String? = null
+    var fullurl: String? = null
+    var thumbnail: WikiThumbnail? = null
+}
---
+++ app/src/main/java/com/kenova/wiki/models/WikiQueryData.kt
@@ -0,0 +1,9 @@
+package com.kenova.wiki.models
+
+/**
+Created by Aaron at 4/20/2020
+Copyright (c) 2020 Kenova
+ **/
+class WikiQueryData {
+    val pages: ArrayList<WikiPage> = ArrayList<WikiPage>()
+}
