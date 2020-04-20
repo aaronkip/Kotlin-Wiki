6968f9e2f49aaaab5f4d2682c873812b2c3dcab9
Akron254 <aaronrono42@gmail.com>
2020-04-20 07:48:19 +0300

    Created Thumbnails model

---
+++ app/src/main/java/com/kenova/wiki/models/WikiThumbnail.kt
@@ -0,0 +1,11 @@
+package com.kenova.wiki.models
+
+/**
+Created by Aaron at 4/20/2020
+Copyright (c) 2020 Kenova
+ **/
+class WikiThumbnail {
+    val source: String? = null
+    val width: Int = 0
+    val height: Int = 0
+}
