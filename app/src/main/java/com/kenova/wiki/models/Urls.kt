638ee7fdb9da7cfc558f9d2525ab879e6b8fe337
Akron254 <aaronrono42@gmail.com>
2020-04-20 07:50:37 +0300

    Created Urls model(Object)

---
+++ app/src/main/java/com/kenova/wiki/models/Urls.kt
@@ -0,0 +1,38 @@
+package com.kenova.wiki.models
+
+/**
+Created by Aaron at 4/20/2020
+Copyright (c) 2020 Kenova
+ **/
+object Urls {
+    val BaseUrl = "https://en.wikipedia.org/w/api.php"
+
+    fun getSearchUrl(term: String, skip: Int, take: Int) : String
+    {
+        return BaseUrl + "?action=query" +
+                "&formatversion=2" +
+                "&generator=prefixsearch" +
+                "&gpssearch=$term" +
+                "&gpslimit=$take" +
+                "&gpsoffset=$skip" +
+                "&prop=pageimages|info" +
+                "piprop=thumbnail|url" +
+                "&pithumbsize=200" +
+                "&pilimit=$take" +
+                "&wbptterms=description" +
+                "&format=json" +
+                "&inprop=url"
+    }
+
+    fun getRandomUrl(take: Int): String{
+        return BaseUrl + "?action=query" +
+                "format=json" +
+                "&formatversion=2" +
+                "&generator=random" +
+                "&gnrnamespace=0" +
+                "&prop=pageimages|info" +
+                "&gnrlimit=$take" +
+                "&inprop=url" +
+                "&pithubmsize=200"
+    }
+}
